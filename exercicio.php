610ef6f6334c13e8d257c13fbff5745b02b3cadf
Milena Carecho <mili_carecho@hotmail.com>
2021-03-23 13:42:56 -0300

    finalização modulo 1

---
+++ exercicio.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link rel="preconnect" href="https://fonts.gstatic.com">
+  <link href="https://fonts.googleapis.com/css2?family=Oswald:wght@200;300;400;500;600;700&display=swap" rel="stylesheet">
+  <link rel="stylesheet" href="assets/css/style.css">
+  <link rel="stylesheet" href="assets/css/exercicio.css">
+
+  <title>Curso PHP</title>
+</head>
+
+<body class="exercicio">
+<header class="cabecalho">
+  <h1>Curso PHP</h1>
+  <h2>Visualização do Exercicio</h2>
+</header>
+    <nav class="navegacao">
+        <a href="#" class="verde">Sem formatação</a>
+        <a href="index.php" class="vermelho">Voltar</a>
+    </nav>
+<main class="principal">
+  <div class="conteudo">
+      
+  </div>
+</main>
+
+<footer class="rodape">
+  COD3R & ALUNOS © <?= date('Y'); ?>
+</footer>
+</body>
+
+</html>
--- index.php
+++ index.php
@@ -14,108 +14,106 @@
   <h1>Curso PHP</h1>
   <h2>Índice dos exercícios</h2>
 </header>
-
-<main class="principal">
-  <div class="conteudo">
-    <nav class="modulos">
-      <div class="modulo verde">
-        <h3>Módulo 01</h3>
-        <ul>
-          <li><a href="#">Exercicio A</a></li>
-          <li><a href="#">Exercicio B</a></li>
-          <li><a href="#">Exercicio C</a></li>
-          <li><a href="#">Exercicio D</a></li>
-        </ul>
-      </div>
-
-      <div class="modulo vermelho">
-        <h3>Módulo 02</h3>
-        <ul>
-          <li><a href="#">Exercicio A</a></li>
-          <li><a href="#">Exercicio B</a></li>
-          <li><a href="#">Exercicio C</a></li>
-          <li><a href="#">Exercicio D</a></li>
-        </ul>
-      </div>
-
-      <div class="modulo azul">
-        <h3>Módulo 03</h3>
-        <ul>
-          <li><a href="#">Exercicio A</a></li>
-          <li><a href="#">Exercicio B</a></li>
-          <li><a href="#">Exercicio C</a></li>
-          <li><a href="#">Exercicio D</a></li>
-        </ul>
-      </div>
-
-      <div class="modulo roxo">
-        <h3>Módulo 04</h3>
-        <ul>
-          <li><a href="#">Exercicio A</a></li>
-          <li><a href="#">Exercicio B</a></li>
-          <li><a href="#">Exercicio C</a></li>
-          <li><a href="#">Exercicio D</a></li>
-        </ul>
-      </div>
-
-      <div class="modulo laranja">
-        <h3>Módulo 05</h3>
-        <ul>
-          <li><a href="#">Exercicio A</a></li>
-          <li><a href="#">Exercicio B</a></li>
-          <li><a href="#">Exercicio C</a></li>
-          <li><a href="#">Exercicio D</a></li>
-        </ul>
-      </div>
-
-      <div class="modulo verde-escuro">
-        <h3>Módulo 06</h3>
-        <ul>
-          <li><a href="#">Exercicio A</a></li>
-          <li><a href="#">Exercicio B</a></li>
-          <li><a href="#">Exercicio C</a></li>
-          <li><a href="#">Exercicio D</a></li>
-        </ul>
-      </div>
-
-      <div class="modulo vermelho-escuro">
-        <h3>Módulo 07</h3>
-        <ul>
-          <li><a href="#">Exercicio A</a></li>
-          <li><a href="#">Exercicio B</a></li>
-          <li><a href="#">Exercicio C</a></li>
-          <li><a href="#">Exercicio D</a></li>
-        </ul>
-      </div>
-
-      <div class="modulo azul-escuro">
-        <h3>Módulo 08</h3>
-        <ul>
-          <li><a href="#">Exercicio A</a></li>
-          <li><a href="#">Exercicio B</a></li>
-          <li><a href="#">Exercicio C</a></li>
-          <li><a href="#">Exercicio D</a></li>
-        </ul>
-      </div>
-
-      <div class="modulo roxo-escuro">
-        <h3>Módulo 09</h3>
-        <ul>
-          <li><a href="#">Exercicio A</a></li>
-          <li><a href="#">Exercicio B</a></li>
-          <li><a href="#">Exercicio C</a></li>
-          <li><a href="#">Exercicio D</a></li>
-        </ul>
-      </div>
-    </nav>
-  </div>
-</main>
-
-<footer class="rodape">
-  COD3R & ALUNOS © <?php date('y'); ?>
-</footer>
-
 <body>
+  <main class="principal">
+    <div class="conteudo">
+      <nav class="modulos">
+        <div class="modulo verde">
+          <h3>Módulo 01</h3>
+          <ul>
+            <li><a href="exercicio.php">Exercicio A</a></li>
+            <li><a href="#">Exercicio B</a></li>
+            <li><a href="#">Exercicio C</a></li>
+            <li><a href="#">Exercicio D</a></li>
+          </ul>
+        </div>
+
+        <div class="modulo vermelho">
+          <h3>Módulo 02</h3>
+          <ul>
+            <li><a href="#">Exercicio A</a></li>
+            <li><a href="#">Exercicio B</a></li>
+            <li><a href="#">Exercicio C</a></li>
+            <li><a href="#">Exercicio D</a></li>
+          </ul>
+        </div>
+
+        <div class="modulo azul">
+          <h3>Módulo 03</h3>
+          <ul>
+            <li><a href="#">Exercicio A</a></li>
+            <li><a href="#">Exercicio B</a></li>
+            <li><a href="#">Exercicio C</a></li>
+            <li><a href="#">Exercicio D</a></li>
+          </ul>
+        </div>
+
+        <div class="modulo roxo">
+          <h3>Módulo 04</h3>
+          <ul>
+            <li><a href="#">Exercicio A</a></li>
+            <li><a href="#">Exercicio B</a></li>
+            <li><a href="#">Exercicio C</a></li>
+            <li><a href="#">Exercicio D</a></li>
+          </ul>
+        </div>
+
+        <div class="modulo laranja">
+          <h3>Módulo 05</h3>
+          <ul>
+            <li><a href="#">Exercicio A</a></li>
+            <li><a href="#">Exercicio B</a></li>
+            <li><a href="#">Exercicio C</a></li>
+            <li><a href="#">Exercicio D</a></li>
+          </ul>
+        </div>
+
+        <div class="modulo verde-escuro">
+          <h3>Módulo 06</h3>
+          <ul>
+            <li><a href="#">Exercicio A</a></li>
+            <li><a href="#">Exercicio B</a></li>
+            <li><a href="#">Exercicio C</a></li>
+            <li><a href="#">Exercicio D</a></li>
+          </ul>
+        </div>
+
+        <div class="modulo vermelho-escuro">
+          <h3>Módulo 07</h3>
+          <ul>x
+            <li><a href="#">Exercicio A</a></li>
+            <li><a href="#">Exercicio B</a></li>
+            <li><a href="#">Exercicio C</a></li>
+            <li><a href="#">Exercicio D</a></li>
+          </ul>
+        </div>
+
+        <div class="modulo azul-escuro">
+          <h3>Módulo 08</h3>
+          <ul>
+            <li><a href="#">Exercicio A</a></li>
+            <li><a href="#">Exercicio B</a></li>
+            <li><a href="#">Exercicio C</a></li>
+            <li><a href="#">Exercicio D</a></li>
+          </ul>
+        </div>
+
+        <div class="modulo roxo-escuro">
+          <h3>Módulo 09</h3>
+          <ul>
+            <li><a href="#">Exercicio A</a></li>
+            <li><a href="#">Exercicio B</a></li>
+            <li><a href="#">Exercicio C</a></li>
+            <li><a href="#">Exercicio D</a></li>
+          </ul>
+        </div>
+      </nav>
+    </div>
+  </main>
+
+  <footer class="rodape">
+    COD3R & ALUNOS © <?= date('Y'); ?>
+  </footer>
 
 </body>
 
