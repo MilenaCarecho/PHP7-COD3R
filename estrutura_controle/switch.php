5ccb39451d93760a24505c74fc7364fec600db1f
Milena Carecho <mili_carecho@hotmail.com>
2021-05-02 21:05:23 -0300

    switch

--- estrutura_controle/switch.php
+++ estrutura_controle/switch.php
@@ -1,8 +1,28 @@
 <div class="titulo">Switch</div>
 
 <?php
+$categoria = "luxo";
+$preco = 0.0;
+$carro = '';
 
-$categoria = 'luxo';
+if($categoria === 'luxo'){
+    $carro = 'Mercedes';
+    $preco = 250000;
+} elseif ($categoria === 'SUV'){
+    $carro = 'Renegade';
+    $preco = 80000;
+} elseif ($categoria === 'Sedan'){
+    $carro = 'Etios';
+    $preco = 55000;
+} else {
+    $carro = 'Mobi';
+    $preco = 33000;
+}
+
+$precoFormatado = number_format($preco, 2, ',', '.');
+echo "<p>Carro: $carro<br>Preço: R$ $precoFormatado</p>";
+
+$categoria = 'SUV';
 switch (strtolower($categoria)){
     case 'luxo':
         $carro = 'Mercedes';
@@ -24,3 +44,4 @@ switch (strtolower($categoria)){
 }
 
 $precoFormatado = number_format($preco, 2, ',', '.');
+echo "<p>Carro: $carro<br>Preço: R$ $precoFormatado</p>";
